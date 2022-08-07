5429403f257d002c309ddb332409c79d6b55dfae
yoshikaz <yoshikaz.matsubara@icloud.com>
2022-08-07 23:03:51 +0900

    init swift package.

---
+++ Sources/Sigmoid/Sigmoid.swift
@@ -0,0 +1,6 @@
+public struct Sigmoid {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SigmoidTests/SigmoidTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Sigmoid
+
+final class SigmoidTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Sigmoid().text, "Hello, World!")
+    }
+}
